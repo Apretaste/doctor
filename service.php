150fe6fc86daf847c779a44b0b450e7a2e4a7126
kumahacker <kumahavana@gmail.com>
2020-01-19 17:43:36 -0500

    fix style

--- service.php
+++ service.php
@@ -1,7 +1,6 @@
 <?php
 
 use Framework\Crawler;
-use Goutte\Client;
 use Apretaste\Request;
 use Apretaste\Response;
 
@@ -19,7 +18,7 @@ class Service
 	/**
 	 * Get a medical article
 	 *
-	 * @param \Apretaste\Request  $request
+	 * @param \Apretaste\Request $request
 	 * @param \Apretaste\Response $response
 	 *
 	 * @throws \Framework\Alert
@@ -34,7 +33,7 @@ class Service
 
 		// respond with error if article not found
 		if (empty($res['artid'])) {
-			$response->setTemplate('message.ejs', ['term' =>$term, 'similars' =>$res['similars']]);
+			$response->setTemplate('message.ejs', ['term' => $term, 'similars' => $res['similars']]);
 			return;
 		}
 
@@ -43,8 +42,8 @@ class Service
 
 		// create array to send info to the view
 		$content = [
-				'term'     => $article['title'],
-				'result'   => $article['body'],
+				'term' => $article['title'],
+				'result' => $article['body'],
 				'similars' => $res['similars']
 		];
 
@@ -56,7 +55,7 @@ class Service
 	/**
 	 * Get a medical article having the article ID
 	 *
-	 * @param \Apretaste\Request  $request
+	 * @param \Apretaste\Request $request
 	 * @param \Apretaste\Response $response
 	 *
 	 * @throws \Framework\Alert
@@ -74,8 +73,8 @@ class Service
 
 		// create an object to send to the template
 		$content = [
-				'term'     => $result['title'],
-				'result'   => $result['body'],
+				'term' => $result['title'],
+				'result' => $result['body'],
 				'similars' => []
 		];
 
@@ -87,7 +86,7 @@ class Service
 	/**
 	 * Get an article based on the ID
 	 */
-	private function getArticleId($term) 
+	private function getArticleId($term)
 	{
 		// lower case and remove tildes for the term
 		$term = trim(strtolower($term));
@@ -96,7 +95,7 @@ class Service
 
 		// load from cache if exists
 		$cache = TEMP_PATH . date('Y') .'_doctor_article_'. md5($term) .'.tmp';
-		if(file_exists($cache)) {
+		if (file_exists($cache)) {
 			/** @var object $content */
 			$content = unserialize(file_get_contents($cache));
 		}
@@ -107,9 +106,14 @@ class Service
 			// get the right URL to pull info
 			$first = $term[0];
 			if (strpos('ABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321', strtoupper($first)) !== false) {
-				if ($first==='x' || $first==='y' || $first==='z') $first = 'xyz';
-				if (strpos('0987654321', $first) !== false) $url = 'https://medlineplus.gov/spanish/healthtopics_0-9.html';
-				else $url = "https://medlineplus.gov/spanish/healthtopics_{$first}.html";
+				if ($first === 'x' || $first === 'y' || $first === 'z') {
+					$first = 'xyz';
+				}
+				if (strpos('0987654321', $first) !== false) {
+					$url = 'https://medlineplus.gov/spanish/healthtopics_0-9.html';
+				} else {
+					$url = "https://medlineplus.gov/spanish/healthtopics_{$first}.html";
+				}
 			}
 
 			// create a crawler
@@ -120,7 +124,7 @@ class Service
 					if (
 						strpos($node->attr('href'), 'https://medlineplus.gov/spanish/') !== false
 						&& strpos($node->attr('href'), 'https://medlineplus.gov/spanish/healthtopics_') === false
-					){
+					) {
 						// lower text
 						$text = strtolower($node->text());
 
@@ -140,14 +144,17 @@ class Service
 						}
 					}
 				});
-			} catch (exception $e) { }
+			} catch (exception $e) {
+			}
 
 			// remove the current ID from the list of similar terms
 			$artid = str_replace(['https://medlineplus.gov/spanish/', '.html', './', 'article'], '', $article);
-			if (isset($similar_terms[$artid])) unset($similar_terms[$artid]);
+			if (isset($similar_terms[$artid])) {
+				unset($similar_terms[$artid]);
+			}
 
 			// return values
-			$content =  ['term' =>$term, 'artid' =>$artid, 'similars' =>$similar_terms];
+			$content = ['term' => $term, 'artid' => $artid, 'similars' => $similar_terms];
 
 			// save cache file
 			file_put_contents($cache, serialize($content));
@@ -167,7 +174,9 @@ class Service
 	{
 		// load from cache if exists
 		$cache = TEMP_PATH  . date('Y') .'_doctor_artid'. md5($artid) .'.tmp';
-		if(file_exists($cache)) $content = unserialize(file_get_contents($cache));
+		if (file_exists($cache)) {
+			$content = unserialize(file_get_contents($cache));
+		}
 
 		// get data from the internet
 		else {
@@ -184,17 +193,19 @@ class Service
 			$title = '';
 			try {
 				$title = Crawler::filter('div.page-title >h1')->text();
-			} catch (exception $e) { }
+			} catch (exception $e) {
+			}
 
 			// get the summary
 			$summary = '';
 			try {
 				$summary = Crawler::filter('div#topic-summary')->html();
 				$summary = preg_replace('#<a.*?>(.*?)</a>#i', '\1', $summary); // remove links
-			} catch (exception $e) { }
+			} catch (exception $e) {
+			}
 
 			// return values
-			$content =  ['title' => $title, 'body' => $summary];
+			$content = ['title' => $title, 'body' => $summary];
 
 			// save cache file
 			file_put_contents($cache, serialize($content));
