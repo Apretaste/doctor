57817de721ce64833b3b6cefe4ac2d5b25612806
kumahacker <kumahavana@gmail.com>
2020-03-28 18:56:08 -0400

    update

--- service.php
+++ service.php
@@ -6,8 +6,14 @@ use Apretaste\Response;
 
 class Service
 {
+
 	/**
 	 * Function executed when the service is called
+	 *
+	 * @param \Apretaste\Request $request
+	 * @param \Apretaste\Response $response
+	 *
+	 * @throws \Framework\Alert
 	 */
 	public function _main(Request $request, Response &$response)
 	{
@@ -23,17 +29,21 @@ class Service
 	 *
 	 * @throws \Framework\Alert
 	 */
-	public function _articulo(Request $request, Response &$response)
+	public function _articulo(Request $request, Response $response)
 	{
 		// lower case and remove tildes for the term
-		$term = trim(strtolower($request->input->data->query));
+		$term = strtolower(trim($request->input->data->query));
 
 		// get the ID for that article
 		$res = $this->getArticleId($term);
 
 		// respond with error if article not found
 		if (empty($res['artid'])) {
-			return $response->setTemplate('message.ejs', ['term' => $term, 'similars' => $res['similars']]);
+			$response->setTemplate('message.ejs', [
+			  'term' => $term,
+			  'similars' => $res['similars']
+			]);
+			return;
 		}
 
 		// get the article for the ID
@@ -41,14 +51,14 @@ class Service
 
 		// create array to send info to the view
 		$content = [
-			"term" => $article['title'],
-			"result" => $article['body'],
-			"similars" => $res['similars']
+			'term' => $article['title'],
+			'result' => $article['body'],
+			'similars' => $res['similars']
 		];
 
 		// create the response
-		$response->setCache("year");
-		$response->setTemplate("basic.ejs", $content);
+		$response->setCache('year');
+		$response->setTemplate('basic.ejs', $content);
 	}
 
 	/**
@@ -66,19 +76,23 @@ class Service
 
 		// treat empty searches
 		if (empty($result)) {
-			return $this->simpleMessage("No hay respuesta", "No encontramos una respuesta a su búsqueda. Por favor trate con otro término.");
+			$response->setTemplate('message.ejs', [
+			  'term' => $request->input->data->query,
+			  'similars' => []
+			]);
+			return;
 		}
 
 		// create an object to send to the template
 		$content = [
-			"term" => $result['title'],
-			"result" => $result['body'],
-			"similars" => []
+			'term' => $result['title'],
+			'result' => $result['body'],
+			'similars' => []
 		];
 
 		// create the response
-		$response->setCache("year");
-		$response->setTemplate("basic.ejs", $content);
+		$response->setCache('year');
+		$response->setTemplate('basic.ejs', $content);
 	}
 
 	/**
@@ -88,27 +102,23 @@ class Service
 	{
 		// lower case and remove tildes for the term
 		$term = trim(strtolower($term));
-		$article = "";
+		$article = '';
 		$similar_terms = [];
 
 		// load from cache if exists
-		$cache = TEMP_PATH . date("Y") . "_doctor_article_" . md5($term) . ".tmp";
+		$cacheName = 'article_'.md5($term);
+		$content = self::loadCache($cacheName);
 
-		if (file_exists($cache)) {
-			/** @var object $content */
-			$content = unserialize(file_get_contents($cache));
-		}
-
-		// get data from the internet
-		else {
+		if ($content === null) {
 			// get the right URL to pull info
 			$first = $term[0];
-			if (strpos("ABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321", strtoupper($first)) !== false) {
-				if ($first == 'x' || $first == 'y' || $first == 'z') {
+			if (stripos('ABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321', $first) !== false) {
+				if ($first === 'x' || $first === 'y' || $first === 'z') {
 					$first = 'xyz';
 				}
-				if (strpos("0987654321", $first) !== false) {
-					$url = "https://medlineplus.gov/spanish/healthtopics_0-9.html";
+
+				if (strpos('0987654321', $first) !== false) {
+					$url = 'https://medlineplus.gov/spanish/healthtopics_0-9.html';
 				} else {
 					$url = "https://medlineplus.gov/spanish/healthtopics_{$first}.html";
 				}
@@ -152,10 +162,10 @@ class Service
 			}
 
 			// return values
-			$content = ["term" => $term, "artid" => $artid, "similars" => $similar_terms];
+			$content = ['term' => $term, 'artid' => $artid, 'similars' => $similar_terms];
 
 			// save cache file
-			file_put_contents($cache, serialize($content));
+			self::saveCache($content, $cacheName);
 		}
 
 		return $content;
@@ -171,14 +181,11 @@ class Service
 	private function getArticle($artid)
 	{
 		// load from cache if exists
-		$cache = TEMP_PATH  . date("Y") . "_doctor_artid" . md5($artid) . ".tmp";
+		$cacheName = "art_id_$artid";
+		$content = self::loadCache($cacheName);
 
-		if (file_exists($cache)) {
-			$content = unserialize(file_get_contents($cache));
-		}
+		if ($content === null) {
 
-		// get data from the internet
-		else {
 			// get the crawler object
 			$url = "https://medlineplus.gov/spanish/$artid.html";
 
@@ -189,27 +196,72 @@ class Service
 			}
 
 			// get the title
-			$title = '';
 			try {
 				$title = Crawler::filter('div.page-title >h1')->text();
+
+				// get the summary
+				$summary = '';
+				try {
+					$summary = Crawler::filter('div#topic-summary')->html();
+					$summary = preg_replace('#<a.*?>(.*?)</a>#i', '\1', $summary); // remove links
+				} catch (exception $e) {
+				}
+
+				// return values
+				$content = ['title' => $title, 'body' => $summary];
+
+				// save cache file
+				self::saveCache($content, $cacheName);
+
 			} catch (exception $e) {
+
 			}
-
-			// get the summary
-			$summary = '';
-			try {
-				$summary = Crawler::filter('div#topic-summary')->html();
-				$summary = preg_replace('#<a.*?>(.*?)</a>#i', '\1', $summary); // remove links
-			} catch (exception $e) {
-			}
-
-			// return values
-			$content = ['title' => $title, 'body' => $summary];
-
-			// save cache file
-			file_put_contents($cache, serialize($content));
 		}
 
 		return $content;
+	}
+
+
+	/**
+	 * Get cache file name
+	 *
+	 * @param $name
+	 *
+	 * @return string
+	 */
+	public static function getCacheFileName($name): string
+	{
+		return TEMP_PATH.'cache/doctor_'.$name.'_'.date('Ymd').'.tmp';
+	}
+
+	/**
+	 * Load cache
+	 *
+	 * @param $name
+	 * @param null $cacheFile
+	 *
+	 * @return bool|mixed
+	 */
+	public static function loadCache($name = 'cache', &$cacheFile = null)
+	{
+		$data = null;
+		$cacheFile = self::getCacheFileName($name);
+		if (file_exists($cacheFile)) {
+			$data = unserialize(file_get_contents($cacheFile));
+		}
+		return $data;
+	}
+
+	/**
+	 * Save cache
+	 *
+	 * @param $name
+	 * @param $data
+	 * @param null $cacheFile
+	 */
+	public static function saveCache($data, $name = 'cache', &$cacheFile = null)
+	{
+		$cacheFile = self::getCacheFileName($name);
+		file_put_contents($cacheFile, serialize($data));
 	}
 }
